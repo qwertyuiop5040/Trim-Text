059d1132d0a7a1eedcae57dcd18fad4c43d9ba61
Hanqing Wang <hwang00@umail.ucsb.edu>
2017-01-19 21:44:26 -0800

    Initial Project Configurations

---
+++ src/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+public class ExampleTest{
+	@Before
+	public void init(){
+		//initialize stuff here
+	}
+	@Test
+	public void oneequalsone(){
+		assertEquals(1,1);
+	}
+}
